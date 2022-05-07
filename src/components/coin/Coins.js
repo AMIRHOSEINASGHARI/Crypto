f3252c3e7ce1ba7383920ca24e5357304c77a6ad
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-05-07 10:21:50 +0430

    Coins-component styles / index.css padding and margin property

--- src/components/coin/Coins.js
+++ src/components/coin/Coins.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
+//styles
+import styles from './Coin.module.css';
+
+
 const Coins = (props) => {
     const {name , image , current_price , price_change_percentage_1h_in_currency , total_volume} = props.coinData
 
     return (
-        <div>
-            <div>
+        <div className={styles.coinContainer}>
+            <div className={styles.coinImageSection}>
                 <img src={image} alt="coin image" />
                 <p>{name}</p>
             </div>
-            <div>
+            <div className={styles.currentPrice}>
                 <p>Current price:</p>
                 <span>$ {current_price.toLocaleString()}</span>
             </div>
-            <div>
+            <div className={styles.lastUpdate}>
                 <p>Last 1hour update:</p>
-                <span>{price_change_percentage_1h_in_currency.toFixed(2)} %</span>
+                <span className={price_change_percentage_1h_in_currency>0 ? styles.green : styles.red}>{price_change_percentage_1h_in_currency.toFixed(2)} %</span>
             </div>
-            <div>
+            <div className={styles.totalVolume}>
                 <p>Total volue:</p>
                 <span>$ {total_volume.toLocaleString()}</span>
             </div>
