9cd9e9e8a4c8840267c4d73c249a0e62d06cdba4
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-05-07 10:09:47 +0430

    search-input for filtering coins

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,6 +7,8 @@ import getCoins from '../../services/api';
 
 const Home = () => {
     const [coins , setCoins] = useState([]);
+    const [inputValue , setInputValue] = useState('')
+
 
     useEffect(() => {
       const fetchCoins = async () => {
@@ -15,13 +17,25 @@ const Home = () => {
     fetchCoins()
     }, [])
 
+    const changeHandler = e => {
+        setInputValue(e.target.value)
+    }
+
+    const searchCoins = coins.filter(item => item.name.toLowerCase().includes(inputValue.toLocaleLowerCase()));
+
+
     return (
         <div>
-            {
-                coins.length ?
-                coins.map(coin => <Coins key={coin.id} coinData={coin} />) :
-                <Loader />
-            }
+            <div>
+                <input type="text" placeholder='Search coin here...' value={inputValue} onChange={changeHandler} />
+            </div>
+            <div>
+                {
+                    searchCoins.length ?
+                    searchCoins.map(coin => <Coins key={coin.id} coinData={coin} />) :
+                    <Loader />
+                }
+            </div>
         </div>
     );
 };
